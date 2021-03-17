354773adf21da55c790d5e9f468f0a8725f37da0
SilentStorm <1449488533qq@gmail.com>
2021-03-17 21:23:20 +0800

    Add 属性委托

---
+++ src/main/kotlin/demo/class/DelegateDemo.kt
@@ -0,0 +1,31 @@
+package demo.`class`
+
+class DelegateDemo {
+}
+
+//Kotlin 支持原生的委托模式
+
+interface Base {
+    fun print()
+}
+
+class BaseImpl(val x: Int) : Base {
+    override fun print() {
+        print(x)
+    }
+}
+
+class Derived(b: Base) : Base by b
+
+//
+
+
+
+
+fun main() {
+    val b = BaseImpl(1)
+    Derived(b).print()
+}
+
+
+
