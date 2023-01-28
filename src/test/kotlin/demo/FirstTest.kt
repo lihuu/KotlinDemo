c9f5df071cf9588ea204da713cd07a9104199dc0
lihu <1449488533qq@gmail.com>
2023-01-28 17:45:59 +0800

    test: test kotlin inner object

--- src/test/kotlin/demo/FirstTest.kt
+++ src/test/kotlin/demo/FirstTest.kt
@@ -15,13 +15,26 @@ class FirstTest {
     
     @Test
     fun testCompanionObject(){
+        //相当于MyClass.Companion.hello()
         val hello = MyClass.hello("lihu")
+        assert(hello=="Hello,lihu")
+    }
+    
+    @Test
+    fun testObject(){
+        val hello = MyClass.obj.hello("lihu")
         assert(hello=="Hello,lihu")
     }
 }
 
 class MyClass{
     companion object {
+        fun hello(name: String):String{
+            return "Hello,$name";
+        }
+    }
+    
+    object obj {
         fun hello(name: String):String{
             return "Hello,$name";
         }
