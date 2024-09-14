70f4d0d97cb068e867670ef765afa472d0e38879
SilentStorm <1449488533qq@gmail.com>
2024-09-15 00:11:39 +0800

    feat: init

--- src/test/kotlin/demo/FirstTest.kt
+++ src/test/kotlin/demo/FirstTest.kt
@@ -1,5 +1,6 @@
 package demo
 
+import demo.`class`.MyClass
 import kotlin.test.Test
 
 /**
@@ -16,16 +17,69 @@ class FirstTest {
     @Test
     fun testCompanionObject() {
         //相当于MyClass.Companion.hello()
-        val hello = MyClass.hello("lihu")
-        assert(hello == "Hello,lihu")
+//        val hello = MyClass.hello("lihu")
+//        assert(hello == "Hello,lihu")
     }
 
     @Test
     fun testObject() {
-        val hello = MyClass.obj.hello("lihu")
-        assert(hello == "Hello,lihu")
+//        val hello = MyClass.obj.hello("lihu")
+//        assert(hello == "Hello,lihu")
+    }
+
+}
+
+/**
+ * 在Kotlin中，open关键字用于修饰类和方法，表示这些类或方法可以被继承或重写。与Java不同，Kotlin中的类和方法默认是不可继承的（即final），需要显式声明为open才能允许子类继承或覆盖。
+ *
+ * open 关键字的作用
+ * 用于类：默认情况下，Kotlin中的类是final的，这意味着它们不能被继承。使用open关键字修饰类后，该类才能被其他类继承。
+ *
+ * 用于方法或属性：Kotlin中类的方法和属性默认也是final的，不能被子类重写。使用open关键字修饰后，方法或属性可以被子类重写。
+ */
+
+abstract class Animal{
+    abstract fun makeSound(): String;
+}
+class Dog: Animal() {
+    override fun makeSound():String {
+        return "Wang"
     }
 }
+
+class Cat: Animal() {
+    override fun makeSound(): String {
+        return "Miao"
+    }
+}
+
+class ReifiedTest{
+   inline fun <reified T: Animal> makeSound(animal: T){
+       when(animal){
+           is Dog -> println(animal.makeSound())
+           is Cat -> println(animal.makeSound())
+           else -> println("Unknown")
+       }
+   }
+
+    @Test
+    fun test(){
+        makeSound(Dog())
+        makeSound(Cat())
+    }
+
+    fun findValue(list:List<String>,target:String){
+        list.find { it == target }?.let {
+            println("Found")
+        }?:println("Not Found")
+    }
+
+    @Test
+    fun testNullable(){
+        findValue(listOf("A","B","C"),"B")
+    }
+}
+
 
 class MyClass {
     companion object {
